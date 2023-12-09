7003cc54567f0c54f85682d195175423d26254c5
bmaiden <b.maiden2012@yahoo.com>
2023-12-09 15:02:46 -0600

    adds functionality for home.ejs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // load express
 const express = require("express");
+const path = require("path");
 
 // create express app
 const app = express();
 
 // configure the app (app.set)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // mount middleware (app.use)
 
@@ -15,7 +18,7 @@ app.get("/", function (req, res) {
 
 // other route directories
 app.get("/home", function (req, res) {
-  res.send("<h1>Home Page</h1>");
+  res.render("home");
 });
 
 // add port for app to listen on
