9a3e5da9a5c4a073ef2654cd0cf2d0a099397446
bmaiden <b.maiden2012@yahoo.com>
2023-12-09 14:30:08 -0600

    adds views directory with home.ejs file

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const express = require("express");
 // create express app
 const app = express();
 
+// configure the app (app.set)
+
+// mount middleware (app.use)
+
 // "root" route directory
 app.get("/", function (req, res) {
   res.send("<h1>Hello World</h1>");
+});
+
+// other route directories
+app.get("/home", function (req, res) {
+  res.send("<h1>Home Page</h1>");
 });
 
 // add port for app to listen on
